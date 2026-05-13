080a3ede43d9cc2e4b653923a1886e22b75b785f
n0thingone <svmunormal@gmail.com>
2026-05-12 22:55:12 -0300

    Disable PWA navigation fallback

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  navigateFallback: null,
 
   buildExcludes: [
     /app-build-manifest\.json$/,
